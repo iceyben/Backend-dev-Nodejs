da138783856768aad1dd3dfd31d2069b1bdf9edb
Benjamin James Mulbah <mulbahb997@gmail.com>
2025-11-20 17:12:58 +0200

    Completed operations and datatypes

---
+++ part1/operations.js
@@ -0,0 +1,57 @@
+//Addition of two numbers
+// To carry a line of code, up and down using the arrow: Put cursor at the line and press Alt + up and down arrow
+
+let addition = 4 + 5;
+
+let score = 102;
+let bonus = 25;
+
+let totalScore = score + bonus;
+
+// subtraction 
+let subtract =9 - 3;
+
+// multiplication
+let mult = 3 * 5;
+
+// division
+let divi = 8 / 2;
+
+//Reminder
+let reminder = 9 % 2;
+
+//exponentiation
+// Example: let say we wat to calculate 2 to the power of 3
+let expo =  2 ** 3;
+
+
+// Increment and Decrement
+//Example of incrementing by 1  
+
+let myScore = 110;
+myScore++; // This is equivalent to myScore = myScore + 1
+
+//Example of decrementing by 1
+let yourScore = 100;
+yourScore--; // This is equivalent to yourScore = yourScore - 1
+
+
+//Comparison Operators
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+
+console.log(num1 == num2); //Equal to 
+console.log(num1 != num3); //True if not equal
+console.log(num1 > num3); //Greater than
+console.log(num1 < num3); //Less than
+console.log(num1 >= num2); //True if greater than or equal to
+console.log(num3 <= num2); //True if less than or equal to
+
+
+
+//&& (AND) operator : You can use it to combine multiple conditions
+
+// || (OR) operator: You can use it to check if at least one condition is true
+
+// ! (NOT) operator: You can use it to negate a condition
