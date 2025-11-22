1f1277bdc1195050ed59944c8a9dcd6f867408d0
Benjamin James Mulbah <mulbahb997@gmail.com>
2025-11-22 15:58:23 +0200

    finished the looping introduction

--- part4/arrayChallenges.js
+++ part4/arrayChallenges.js
@@ -37,4 +37,74 @@ console.log(`The first tea is: ${firstTea}`);
     let citiesvisited = ["Mumbai", "Sydney"];
     citiesvisited.push("Berlin");
 
-   console.log(citiesvisited)
+   console.log(citiesvisited);
+
+
+/* 
+   5. You have an array named 'teaOrders'with 'chai', 'iced tea', 'matcha', and 'earl grey'.
+   Remove the last element of the array usig the pop method and store it in a variable named lastOrder.
+ */
+
+   let teaOrders = ["chai","iced tea", "matcha", "earl grey"];
+   const lastOrder = teaOrders.pop(teaOrders);
+   console.log(`Your last order is: ${lastOrder}`);
+   console.log(teaOrders);
+
+
+/* 
+   6. You have an array named 'popularTeas' containing "green tea", "oolong tea", and "chai".
+   Create a soft copy of this array named softCopyTeas
+*/
+
+ let popularTeas = ["green tea", "oolong tea", "chai"];
+ const softCopyTeas = popularTeas;
+ console.log(popularTeas)
+ console.log(softCopyTeas)
+
+
+/* 
+   7. You have an array named 'topCities' containing 'Berlin', 'Singapore', and 'New York'.
+   Create a hard copy of this array named hardCopyCities
+ */
+ console.log("///////// Top Cities /////////");
+   let topCities = ["Berlin", "Singapore", "New York"];
+   let hardCopyCities = [...topCities];
+   topCities.pop();
+   console.log(`Your original copy: ${topCities}`)
+    console.log(`Your hard copy: ${hardCopyCities}`)
+  
+
+/* 
+   8. You have two arrays: 'europeanCities' containing 'Paris' and 'Rome', and 'asianCities' containing "tokyo" and "Bangkok"
+   Merge these two arrays into a new array named 'worldCities'.
+ */
+
+   console.log("/////// HardCopy ////////")
+
+   let europeanCities = ["Paris", "Rome"];
+   let asianCities = ["Tokyo", "Bangkok"];
+   let worldCities = [...europeanCities,...asianCities];
+   console.log(`Array join using spread: ${ worldCities}`);
+     worldCities = europeanCities.concat(asianCities);
+   console.log(`Array join using concat(): ${ worldCities}`);
+   
+
+/* 
+   9. You have an array named 'teaMenu' containing 'masala chai', 'oolong tea', 'green tea', and 'earl grey'. 
+   Find the length of the array and store it in a variable named 'menuLength'.
+ */
+
+   let teaMenu = ["masala chai", "oolong tea", "green tea", "earl grey"];
+   let menuLength = teaMenu.length;
+   console.log(menuLength);
+
+/* 
+    10. You have an array named 'cityBucketList' containing 'Kyoto', "London", "Cape Town", and "Vancouver". 
+    Check if 'London'is in the array and store the result in a variable named isLondonInList.
+ */
+
+    let cityBucketList = ["Kyoto", "London", "Cape Town", "Vancouver"];
+   let isLondonInList = cityBucketList.includes("London");
+   console.log(isLondonInList);
+
+   
---
+++ part4/loopChallenges.js
@@ -0,0 +1,24 @@
+/* 1. Write a "while" loop that calculates the sum of all numbers from 1 to 5 and stores the result in a variable named "sum". */
+/* let i = 1;
+let sum = 0;
+
+while(i <= 5){
+   sum = sum + i;
+   i++;
+}
+console.log(sum);
+ */
+/* 2. Write a "while" loop that counts down from 5 to 1 and stores the numbbers in an array named "countdown".
+ */
+
+let i= 5;
+let countdown = []
+while (i >= 1) {
+    countdown.push(i)
+    i--;
+}
+
+console.log(countdown)
+
+
+
