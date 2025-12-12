11d02cd1e28327edfaa836e1f3ff8c2777f881ed
Benjamin James Mulbah <mulbahb997@gmail.com>
2025-12-12 06:45:27 -0800

    I will stop here for the day and continue. I've started building basic server

--- section4/Basic-Server/index.js
+++ section4/Basic-Server/index.js
@@ -3,7 +3,7 @@ const http = require("node:http");
 const server = http.createServer((req,res)=>{
     console.log("I got an incoming request");
     res.writeHead(200);
-    res.end()
+    res.end("<h1>This is an example server running</h1>");
 });
 
 const PORT = 8000;
