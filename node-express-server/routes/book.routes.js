f6a22579bce33f75ae1db416dc13d998948b9519
Benjamin James Mulbah <mulbahb997@gmail.com>
2025-12-22 03:55:05 +0200

    I will stop here for the night and continue tomorrow

--- node-express-server/routes/book.routes.js
+++ node-express-server/routes/book.routes.js
@@ -1,71 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("../controller/books.controller")
 
 const Books = require("../models/books")
 
 
-router.get("/", (req,res)=>{
-     return res.json(Books);
-})
+router.get("/",controller.getAllBooks);
 
-router.get("/:id", (req,res)=>{
-      const id = parseInt(req.params.id);
-      const index = Books.find((e) =>e.id === id); 
-      
-     if(!index){
-            return res.status(404).json({error: `Book with the ID ${id} is not available`});
-     }else{
-            return res.json(index);
-     }
-});
+router.get("/:id", controller.getBooksById);
 
-router.post("/", (req,res)=>{
-      const {title, author} = req.body;
-
-      //This is to validate title and make sure it is entered
-      if(!title || title === ""){
-            return res.status(400).json({error: `Title is required`})
-      }
-
-       //This is to validate author and make sure the field is entered
-      if(!author || author === ""){
-            return res.status(400).json({error: `Author is required`});
-      }
+router.post("/", controller.createBook);
 
 
-
-      //Now you have to create a new book object
-      const id = Books.length + 1;
-      const book = {id, title, author}
-      Books.push(book);
-
-      return res.status(201).json({message: `Book for created successfully! id:${id}`});
-})
-
-
-router.patch("/:id", (req, res)=>{
-      const id =parseInt(req.params.id);
-
-      if(isNaN(id)){
-            return res.status(404).json({error: `id must me a number`});
-      }
-      const book = Books.find((b) => b.id === id);
-
-      if(!book){
-            return res.status(404).json({error: `Book with ID: ${id} not found`});
-      }
-
-      // Update the book details now
-      if(req.body.title){
-            book.title = req.body.title;
-      }
-      if(req.body.author){
-            book.author = req.body.author;
-      }
-
-      return res.json({message: `Book was updated successfully`, data : book});
-
-})
+router.patch("/:id", controller.updateBook)
 
 router.delete("/:id", (req,res)=>{
       const id = parseInt(req.params.id);
